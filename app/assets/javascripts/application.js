2ab630af641789b78de7b73ad5c30167ebf16704
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-15 13:26:17 -0500

    update gemfile, add bootstrap and material to js and scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require bootstrap-material-design
+//= require materialize-sprockets
+//= require bootstrap-sprockets
 //= require_tree .
