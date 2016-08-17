46a2c5c02e48e6e9a7e930794fd38e62df3f8e1b
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-16 19:24:20 -0500

    List controller CRUD

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,50 @@
 class ListsController < ApplicationController
+  def new
+      @list = List.new
+    end
+
+    def create
+      @list=List.create(list_params)
+      if @list.save
+        redirect_to dashboard_path, notice: "List Successfully Added"
+      else
+        render :new
+      end
+    end
+
+    def index
+      @lists = List.all
+    end
+
+    def show
+      @list = List.find_by id: params[:id]
+    end
+
+    def edit
+      @list = List.find_by id: params[:id]
+    end
+
+    def update
+      @list = List.find_by id: params[:id]
+      if @list.update(list_params)
+        redirect_to dashboard_path, notice: "List Details Successfully Updated"
+      else
+        render :edit
+      end
+    end
+
+    def delete
+        @list = List.find_by id: params[:id]
+        @list.destroy
+        redirect_to dashboard_path, notice: 'List Details Removed'
+    end
+
+    private
+
+    def list_params
+      params.require(:list).permit()
+    end
+
+  end
+
 end
