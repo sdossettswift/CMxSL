5c6cbc44214c6e5f2770e70806dd254de7991b7d
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-16 19:23:02 -0500

    Groups controller CRUD

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,48 @@
 class GroupsController < ApplicationController
-end
+  def new
+      @group = Group.new
+    end
+
+    def create
+      @group=Group.create(group_params)
+      if @group.save
+        redirect_to dashboard_path, notice: "Group Successfully Added"
+      else
+        render :new
+      end
+    end
+
+    def index
+      @groups = Group.all
+    end
+
+    def show
+      @group = Group.find_by id: params[:id]
+    end
+
+    def edit
+      @group = Group.find_by id: params[:id]
+    end
+
+    def update
+      @group = Group.find_by id: params[:id]
+      if @group.update(group_params)
+        redirect_to dashboard_path, notice: "Group Details Successfully Updated"
+      else
+        render :edit
+      end
+    end
+
+    def delete
+        @group = Group.find_by id: params[:id]
+        @group.destroy
+        redirect_to dashboard_path, notice: 'Group Details Removed'
+    end
+
+    private
+
+    def group_params
+      params.require(:group).permit()
+    end
+
+  end
