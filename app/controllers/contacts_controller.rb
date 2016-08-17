1be2ea4653b8bd651c7a5481d4b99c4f4294d589
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-16 19:26:19 -0500

    List strong params

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -42,7 +42,7 @@ class ContactsController < ApplicationController
     private
 
     def contact_params
-      params.require(:contact).permit()
+      params.require(:contact).permit(:last_name, :first_name, :email, :phone, :company, :address1, :address2, :city, :state, :zip,:notes, :status, :interest, :list_id, :list )
     end
 
   end
