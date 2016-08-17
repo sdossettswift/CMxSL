bd406cda99707938e2ec25fdfa001c92f7bb3b0c
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-16 19:21:54 -0500

    contacts controller and events controller - CRUD

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,48 @@
 class ContactsController < ApplicationController
-end
+  def new
+      @contact = Contact.new
+    end
+
+    def create
+      @contact=Contact.create(contact_params)
+      if @contact.save
+        redirect_to dashboard_path, notice: "Contact Successfully Added"
+      else
+        render :new
+      end
+    end
+
+    def index
+      @contacts = Contact.all
+    end
+
+    def show
+      @contact = Contact.find_by id: params[:id]
+    end
+
+    def edit
+      @contact = Contact.find_by id: params[:id]
+    end
+
+    def update
+      @contact = Contact.find_by id: params[:id]
+      if @contact.update(contact_params)
+        redirect_to dashboard_path, notice: "Contact Details Successfully Updated"
+      else
+        render :edit
+      end
+    end
+
+    def delete
+        @contact = Contact.find_by id: params[:id]
+        @contact.destroy
+        redirect_to dashboard_path, notice: 'Contact Details Removed'
+    end
+
+    private
+
+    def contact_params
+      params.require(:contact).permit()
+    end
+
+  end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,48 @@
 class EventsController < ApplicationController
-end
+  def new
+      @event = Event.new
+    end
+
+    def create
+      @event=Event.create(event_params)
+      if @event.save
+        redirect_to dashboard_path, notice: "Event Successfully Added"
+      else
+        render :new
+      end
+    end
+
+    def index
+      @events = Event.all
+    end
+
+    def show
+      @event = Event.find_by id: params[:id]
+    end
+
+    def edit
+      @event = Event.find_by id: params[:id]
+    end
+
+    def update
+      @event = Event.find_by id: params[:id]
+      if @event.update(event_params)
+        redirect_to dashboard_path, notice: "Event Details Successfully Updated"
+      else
+        render :edit
+      end
+    end
+
+    def delete
+        @event = Event.find_by id: params[:id]
+        @event.destroy
+        redirect_to dashboard_path, notice: 'Event Details Removed'
+    end
+
+    private
+
+    def event_params
+      params.require(:event).permit()
+    end
+
+  end
