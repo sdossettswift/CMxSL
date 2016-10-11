0668bded62b8e513780b5455f9acfe753c0382dc
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-11 18:13:48 -0500

    import function works

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,15 @@
 Rails.application.routes.draw do
-
-
-  get 'meetup/new'
-
-  get 'meetup/edit'
-
-  get 'meetup/index'
-
-  get 'meetup/show'
-
-  CMxSL::Application.routes.draw do
-  
-  get 'meetup/new'
-
-  get 'meetup/edit'
-
-  get 'meetup/index'
-
-  get 'meetup/show'
-
-    resources :contacts, only: [:index]
-    resources :lists, only: [:index]
-    get 'dashboard' => 'dashboard#welcome', as: :dashboard
-
-  end
-
-    resources :lists
-    resources :contacts
-    resources :users
-    resources :events
-    resources :groups
-
-
-
-    get 'sign_in' => 'sessions#new', as: :sign_in
-    post 'sign_in' => 'sessions#create'
-    delete 'sign_in' => 'sessions#delete', as: :end_session
-    get 'sign_out' => 'sessions#delete', as: :sign_out
-
-    root 'welcome#hello'
+  resources :lists
+  resources :contacts do
+        collection {post :import}
+    end
+  resources :users
+  resources :events
+  resources :groups
+  get 'sign_in' => 'sessions#new', as: :sign_in
+  post 'sign_in' => 'sessions#create'
+  delete 'sign_in' => 'sessions#delete', as: :end_session
+  get 'sign_out' => 'sessions#delete', as: :sign_out
+  get 'dashboard' => 'dashboard#welcome', as: :dashboard
+  root 'welcome#hello'
 end
