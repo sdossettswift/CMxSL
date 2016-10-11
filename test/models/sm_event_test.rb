3febe2521e7d852b4be3c2268857fa0c9c7a7952
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-11 12:57:44 -0500

    remove blank tests

--- test/models/sm_event_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe SmEvent do
-  let(:sm_event) { SmEvent.new }
-
-  it "must be valid" do
-    value(sm_event).must_be :valid?
-  end
-end
--- test/models/sm_group_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe SmGroup do
-  let(:sm_group) { SmGroup.new }
-
-  it "must be valid" do
-    value(sm_group).must_be :valid?
-  end
-end
